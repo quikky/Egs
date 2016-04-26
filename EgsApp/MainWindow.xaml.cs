719ba3b36bc7dc04565b379393fb46880aefb2d1
Quikky <765pro.gz@gmail.com>
2016-04-26 22:57:39 +0900

    HttpWebRequestでデータ取得

--- EgsApp/MainWindow.xaml.cs
+++ EgsApp/MainWindow.xaml.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Net;
+using System.Web;
+using System.Text;
 
 namespace EgsApp
 {
@@ -22,6 +15,45 @@ namespace EgsApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+            // API叩く用URL
+            var url = string.Format("{0}{1}"
+                , "http://api.hitonobetsu.com/surl/open?url="
+                , textBox1.Text);
+
+            // リクエスト
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            // 速度対策
+            req.Proxy = null;
+
+            HttpWebResponse response = null;
+            try
+            {
+                //レスポンスの取得
+                response = (HttpWebResponse)req.GetResponse();
+                Stream resStream = response.GetResponseStream();
+                StreamReader sr = new StreamReader(resStream);
+                string EgsData = sr.ReadToEnd();
+                sr.Close();
+                Console.WriteLine(EgsData);
+                label1.Content = EgsData.ToString();
+                resStream.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error" + ex.ToString());
+                return;
+            }
+            finally
+            {
+                if (response != null)
+                    response.Close();
+            }
         }
 
 
